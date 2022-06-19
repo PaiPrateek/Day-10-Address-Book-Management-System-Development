af86cd21a5570db70862b76ad2fe6e5609265c92
Prateek Pai <paiprateek123@gmail.com>
2022-06-19 22:34:39 +0530

    Ability to Add Multiple Person to Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,7 +6,38 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Problem");
+            Console.WriteLine("Enter the Size of the Address Book:");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n                                        \n");
+
+            Console.WriteLine("Size of the Address Book is :" + size);
+            Console.WriteLine("...................................");
+            Console.WriteLine("\n                                        \n");
+
+            Console.WriteLine("Press Enter to Add the COntact in Address Book ");
+            string Press = Console.ReadLine();
+
+            AddressBook addressBook = new AddressBook(size);
+            addressBook.AddNewContact();
+
+            Console.WriteLine("The Details of the First Contact in Address Book is:");
+            addressBook.PrintAllContact();
+            Console.WriteLine("...................................");
+
+            Console.WriteLine("Press 1 to Add Contact to address Book");
+            int Input = int.Parse(Console.ReadLine());
+            Console.WriteLine("...................................");
+
+            if (Input==1)
+            {
+                addressBook.AddNewContact();
+                Console.WriteLine("...................................");
+                Console.WriteLine("Contact is Added to Address Book Successfylly");
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid input");
+            }
         }
     }
 }
